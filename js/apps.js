e3ecf1561762df1a3ceeac9bf30754f59f68f638
Bananestar <s.vanderlinden13@gmail.com>
2022-06-09 11:08:28 +0200

    non-fonctionnel

--- js/apps.js
+++ js/apps.js
@@ -3,120 +3,281 @@
  *
  * Copyright (c) 2022 Simon Vander Linden
  */
-// const elecEl = document.getElementById("election");
-// const box = document.getElementById("box");
-// const btn = document.getElementById("start");
 
-// function start() {
-//   box.parentNode.removeChild(box);
-//   buttonAdd();
-// }
+const btnEl = document.getElementById("startButton");
+const btnPlayEl = document.getElementById("play");
+const btnResetEl = document.getElementById("reset");
+const btnStopEl = document.getElementById("stop");
+const btnAddUserEl = document.getElementById("addUser");
+const starterContainerEl = document.getElementById("starterContainer");
+const containerUserEl = document.getElementById("containerUser");
+const listUserEl = document.getElementById("listUser");
+const WinMessageEl = document.getElementById("WinMessage");
 
-// box.addEventListener("click", start);
+let users = [];
+let time = 2;
+let limitTime = 350;
+let counterLoopAnimation = 0;
+let catchUser = "catchUser";
+let userChoosed = null;
+let intervalChoosed;
 
-// function buttonAdd() {
-//   const btnAdd = document.createElement("button");
-//   const btnContent = document.createTextNode("+");
-//   btnAdd.classList.add("btn", "btn-primary", "btn-green");
-//   btnAdd.setAttribute("id", "btnAdd");
-//   btnAdd.appendChild(btnContent);
-//   elecEl.appendChild(btnAdd);
-// }
+listUserEl.style.display = "none";
+listUserEl.style.opacity = 0;
 
-// function addUser() {
-//     console.log('rr');
-//   prompt("Ajouter un joueur");
-// }
+btnDisabled();
+checker(false);
 
-// // const btnAdd = document.getElementById("btnAdd");
-// // do {
+document.addEventListener("DOMContentLoaded", function () {
+  btnEl.addEventListener("click", function () {
+    loadGame();
+  });
 
-// // } while (!btnAdd);
-// // btnAdd.addEventListener("click", addUser);
+  btnAddUserEl.addEventListener("click", function () {
+    addUser();
+  });
 
-const boxEl = document.getElementById("box");
-const btnEl = document.getElementById("start");
+  btnPlayEl.addEventListener("click", function () {
+    //check des btn
+    checker();
+  });
 
-function remove() {
-  // remove element
-  boxEl.textContent = "";
+  btnResetEl.addEventListener("click", function () {
+    reset();
+  });
 
-  //btn add
-  let btnAddEl = document.createElement("button");
-  const iEl = document.createElement("i");
+  btnStopEl.addEventListener("click", function () {
+    stopGame();
+  });
+});
 
-  btnAddEl.classList.add("btn", "btn-primary");
-  btnAddEl.setAttribute("id", "btnAdd");
-
-  iEl.classList.add("fa-duotone", "fa-plus");
-
-  btnAddEl.appendChild(iEl);
-  boxEl.appendChild(btnAddEl);
-  boxEl.removeEventListener("click", remove);
-
-  // add listen for addUser
-  btnAddEl = document.getElementById("btnAdd");
-  btnAddEl.addEventListener("click", addUser);
-
-  //
-}
-
-boxEl.addEventListener("click", remove);
-
-// add User
-
-const users = [];
-
+// Add User
 function addUser() {
-  const user = prompt("Add User");
-//   let nb = 0;
+  let name = userName();
 
-//   for (let i = 0; i < users.length; i++) {
-//     if (users.find(element => element == user)) {
-//       nb++;
-//       console.log(nb);
-//     }
-//   }
+  if (name != null) {
+    users.push({ name: name });
 
-//   if (users.find(element => element == user)) {
-      
-//     user += nb;
-//     console.log(user);
-//   }
+    const articleEl = document.createElement("article");
+    articleEl.classList.add("card");
+    articleEl.id = name;
 
-  const userID = user + Date.now();
-  users.push(userID);
+    const divEl = document.createElement("div");
+    divEl.classList.add("content");
 
-  const divUserEl = document.createElement("div");
+    const titleEl = document.createElement("h4");
+    const nameEl = document.createTextNode(name);
 
-  const divUserContent = document.createTextNode(user);
+    const btnRemoveEl = document.createElement("button");
+    const btnRemoveValue = document.createTextNode("-");
+    btnRemoveEl.classList.add("btn", "btn-red");
 
-  divUserEl.appendChild(divUserContent);
-  divUserEl.appendChild(ajoute_btn_minus(userID));
-  divUserEl.setAttribute("id", userID);
-  boxEl.appendChild(divUserEl);
+    // btnRemoveEl.addEventListener("click", removeUser(name));
+    btnRemoveEl.onclick = () => {
+      removeUser(name);
+    };
+
+    containerUserEl.appendChild(articleEl);
+    articleEl.appendChild(divEl);
+    btnRemoveEl.appendChild(btnRemoveValue);
+    divEl.appendChild(titleEl, btnRemoveEl);
+    titleEl.appendChild(nameEl);
+  }
+  checker(false);
 }
 
-const ajoute_btn_minus = function (userID) {
-  let temp = document.createElement("button");
+// Prompt + regex Name + nb
+function userName() {
+  let regex =
+    /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/;
+  let name;
 
-  temp.className = "minus_btn";
+  do {
+    name = prompt(
+      `Nom du nouveau joueur.\nLes caractères spéciaux ne sont pas admis`
+    );
 
-  let tempIcon = document.createElement("i");
+    name = name.trim();
 
-  tempIcon.textContent = "-";
+    // multi user
+    if (name.match(regex)) {
+      nbUser = 1;
+      users.forEach((e) => {
+        if (name.toLowerCase() == e.name.toLowerCase()) {
+          nbUser++;
+          name = name + nbUser;
+        }
+      });
+    }
+  } while (!name.match(regex));
 
-  temp.appendChild(tempIcon);
+  return name;
+}
 
-  temp.onclick = () => {
-    removeUser(userID);
-  };
+function loadGame() {
+  starterContainerEl.style.opacity = 0;
+  starterContainerEl.style.display = "none";
 
-  return temp;
-};
+  listUserEl.style.display = null;
+  listUserEl.style.opacity = null;
+}
 
-function removeUser(userID) {
-  users.splice(users.indexOf(userID), 1);
-  const divUserEl = document.getElementById(userID);
-  divUserEl.remove();
+// check btn for begin game
+function checker(wantToPlay = true) {
+  if (users.length >= 2) {
+    btnPlayEl.disabled = false;
+    btnPlayEl.classList.add("btn", "btn-red");
+  } else {
+    btnPlayEl.removeAttribute("class");
+    btnPlayEl.disabled = true;
+  }
+
+  if (wantToPlay && !btnPlayEl.disabled) {
+    beginTheGame();
+  }
+}
+
+// begin game --> disabled all btn
+function beginTheGame() {
+  const allBtn = document.querySelectorAll("button");
+  btnAddUserEl.style.display = "none";
+  btnAddUserEl.disabled = true;
+
+  btnResetEl.style.display = null;
+  btnStopEl.style.display = null;
+
+  for (let i = 0; i < allBtn.length; i++) {
+    allBtn[i].classList.remove("btn", "btn-red", "btn-green");
+    allBtn[i].disabled = true;
+  }
+  choosingUser();
+}
+
+// time addition
+function addTime() {
+  return (time = time * 1.1);
+}
+
+//random User
+function choosingUser() {
+  const userAlreadySelected = document.getElementsByClassName(catchUser);
+
+  if (userAlreadySelected[0] != undefined) {
+    userAlreadySelected[0].classList.remove(catchUser);
+  }
+
+  let nbIndex = Math.floor(Math.random() * users.length);
+  const userSelected = users[nbIndex].name;
+  const articleSelected = document.getElementById(userSelected);
+  articleSelected.classList.add(catchUser);
+
+  if (time > limitTime) {
+    userChoosed = userSelected;
+    intervalChoosed = setInterval(animateUser, 500);
+  } else {
+    setTimeout(choosingUser, addTime());
+  }
+}
+
+//animation carrousel
+function animateUser() {
+  const userSelected = document.getElementById(userChoosed);
+  const classOfUser = document.getElementsByClassName(catchUser);
+
+  if (counterLoopAnimation < 6) {
+    if (classOfUser[0] != undefined) {
+      userSelected.classList.remove(catchUser);
+    } else {
+      userSelected.classList.add(catchUser);
+    }
+
+    counterLoopAnimation++;
+  } else {
+    clearInterval(intervalChoosed);
+    btnResetEl.removeAttribute("disabled");
+    btnResetEl.classList.add("btn", "btn-green");
+
+    btnStopEl.removeAttribute("disabled");
+    btnStopEl.classList.add("btn", "btn-red");
+
+    const titleWinnerEl = document.createElement("h3");
+    titleWinnerEl.classList.add("RightToLeft");
+    titleWinnerEl.textContent = `Félicitation ${userChoosed}, tu es l'élu!!`;
+    messageWinner.append(titleWinnerEl);
+  }
+}
+
+//remove User
+function removeUser(userName) {
+  const articleEl = document.getElementById(userName);
+  articleEl.remove();
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].name == userName) {
+      users.splice(i, 1);
+      break;
+    }
+    checker(false);
+  }
+}
+
+//disabled btn Reset and Stop
+
+function btnDisabled() {
+  btnResetEl.classList.remove("btn", "btn-red");
+  btnResetEl.style.disabled = true;
+  btnResetEl.style.display = "none";
+
+  btnStopEl.classList.remove("btn", "btn-red");
+  btnStopEl.style.disabled = true;
+  btnStopEl.style.display = "none";
+}
+
+
+function reset() {
+  resetGame()
+  checker()
+}
+
+function resetGame(params) {
+  const userAlreadySelected = document.getElementsByClassName(catchUser);
+  const messageAnimated = document.getElementsByClassName("RightToLeft");
+
+  if (userAlreadySelected[0] != undefined) {
+    userAlreadySelected[0].classList.remove(catchUser);
+  }
+
+  if (messageAnimated[0] != undefined) {
+    messageAnimated[0].remove();
+  }
+
+  counterLoopAnimation = 0;
+  time = 2;
+  userChoosed = null;
+}
+
+function stopGame() {
+  resetGame()
+  btnAddUserEl.style.display = null;
+  btnAddUserEl.classList.add("btn", "btn-green");
+  btnAddUserEl.removeAttribute("disabled");
+    btnPlayEl.classList.add("btn", "btn-red");
+    btnPlayEl.removeAttribute("disabled");
+
+    disableRetryAndStop();
+
+    const allBtn = document.querySelectorAll("article button");
+    for (let i = 0; i < allBtn.length; i++) {
+      allBtn[i].classList.add("btn", "btn-red");
+      allBtn[i].removeAttribute("disabled");
+    }
+}
+
+function disableRetryAndStop() {
+  btnResetEl.classList.remove("btn", "btn-red");
+  btnResetEl.style.disabled = true;
+  btnResetEl.style.display = "none";
+
+  btnStopEl.classList.remove("btn", "btn-red");
+  btnStopEl.style.disabled = true;
+  btnStopEl.style.display = "none";
 }
